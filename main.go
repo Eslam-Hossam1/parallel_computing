4cc92403fd10a60067856eb9d7a364544b40cadb
Eslam Hossam <129710630+Eslam-Hossam1@users.noreply.github.com>
2024-11-03 11:28:48 +0200

    section 4 : getMoney func , random ,  error handling , infinty loop and infinty listening for channel , close channel after infinity listening, some knowledge

--- main.go
+++ main.go
@@ -2,13 +2,46 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
+func getMoney(ch chan int) {
+	//Accept number of trials
+	//محاولات
+	var trials int
+	fmt.Println("Enter Number of Trails: ")
+	if _, err := fmt.Scan(&trials); err != nil {
+		fmt.Println("Please Provide Integer number")
+		return
+	} // Error handling
+	rand.Seed(time.Now().UnixNano()) //Pseudu Random
+	for i := 1; i <= trials; i++ {
+		amount := rand.Intn(500)
+		ch <- amount
+	}
+	//End sending
+	close(ch)
+
+	//for knowledge
+	// 	infinty loop like while
+	// 	for{
+	// 		if x > 5{
+	// 			break
+	// 		}
+	// 	}
+}
+
 func main() {
-	channel := make(chan int, 2) //buffered with capacity = 2
-	channel <- 500 //sendind
-	channel<-600
-	fmt.Println(<-channel) //reciving
-	fmt.Println(<-channel)
+	channel := make(chan int)
+	go getMoney(channel)
+	for msg := range channel { // check whether channel (sending)
+		fmt.Printf("You've won: %d$\n", msg)
+	}
+
+	// channel <- 500 //sending
+	// channel<-600
+	// fmt.Println(<-channel) //reciving
+	// fmt.Println(<-channel)
 
 }
