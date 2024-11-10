1aaa45d02869b143d473b160b0de84b43fa5c234
Eslam Hossam <129710630+Eslam-Hossam1@users.noreply.github.com>
2024-11-10 13:20:53 +0200

    slice

--- main.go
+++ main.go
@@ -5,8 +5,12 @@ import(
 )
 
 func main() {
-	number := [5]int{1,2,3,4,5}
+	number := [5]int{1,2,3,4,5} // [1,2,3,4,5]
 	fmt.Println(number)
+
+	slice := number[1:4] // len = 3, capacity = from 1 (first number  or initial whatever) to the end of original array = 4
+	
+	fmt.Println(slice)
 
 }
 
