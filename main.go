6c65705e7b26e446c548daf31d394b634254e2d3
Eslam Hossam <129710630+Eslam-Hossam1@users.noreply.github.com>
2024-11-10 12:53:54 +0200

    wait group another way of sync threads with main func

--- main.go
+++ main.go
@@ -1,47 +1,20 @@
 package main
-
-import (
+import(
 	"fmt"
-	"math/rand"
-	"time"
+	"sync"
 )
 
-func getMoney(ch chan int) {
-	//Accept number of trials
-	//محاولات
-	var trials int
-	fmt.Println("Enter Number of Trails: ")
-	if _, err := fmt.Scan(&trials); err != nil {
-		fmt.Println("Please Provide Integer number")
-		return
-	} // Error handling
-	rand.Seed(time.Now().UnixNano()) //Pseudu Random
-	for i := 1; i <= trials; i++ {
-		amount := rand.Intn(500)
-		ch <- amount
+func main() {
+	var wg sync.WaitGroup
+	for i := 1; i<=5; i++{	
+	wg.Add(1)
+	go printNumber(i,&wg)
 	}
-	//End sending
-	close(ch)
+	wg.Wait()
 
-	//for knowledge
-	// 	infinty loop like while
-	// 	for{
-	// 		if x > 5{
-	// 			break
-	// 		}
-	// 	}
 }
 
-func main() {
-	channel := make(chan int)
-	go getMoney(channel)
-	for msg := range channel { // check whether channel (sending)
-		fmt.Printf("You've won: %d$\n", msg)
-	}
-
-	// channel <- 500 //sending
-	// channel<-600
-	// fmt.Println(<-channel) //reciving
-	// fmt.Println(<-channel)
-
+func printNumber(num int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println(num)
 }
